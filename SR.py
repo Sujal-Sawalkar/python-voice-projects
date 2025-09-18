975d3277004571295c9dbf177e991a56a553db0f
Sujal-Sawalkar <sujalsawalkar2005@gmail.com>
2025-09-18 23:04:59 +0530

    Add files via upload

---
+++ SR.py
@@ -0,0 +1,18 @@
+import sounddevice as sd
+import speech_recognition as sr
+import numpy as np
+
+recognizer = sr.Recognizer()
+
+print("Speak something...")
+duration = 5  
+fs = 44100    
+recording = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='int16')
+sd.wait()
+
+audio_data = sr.AudioData(recording.tobytes(), fs, 2)
+try:
+    text = recognizer.recognize_google(audio_data)
+    print("You said:", text)
+except sr.UnknownValueError:
+    print("Sorry, could not understand.")
